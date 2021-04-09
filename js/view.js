7a070d22845e86426cce3f8170d9014c3b7c63c0
willo11 <47759930+willo11@users.noreply.github.com>
2021-04-09 10:59:05 -0500

    filters

--- js/view.js
+++ js/view.js
@@ -1,16 +1,19 @@
 import AddTodo from "./components/add-todo.js";
 import Modal from "./components/modal.js";
+import Filters from "./components/filters.js";
 export default class View {
   constructor() {
     this.model = null;
     this.table = document.getElementById("table");
     this.addTodoForm = new AddTodo();
     this.modal = new Modal();
+    this.filters = new Filters();
 
     this.addTodoForm.onClick((title, description) =>
       this.addTodo(title, description)
     );
     this.modal.onClick((id, values) => this.editTodo(id, values));
+    this.filters.onClick((filters) => this.filter(filters));
   }
 
   setModel(model) {
@@ -20,6 +23,34 @@ export default class View {
   render() {
     const todos = this.model.getTodos();
     todos.forEach((todo) => this.createRow(todo));
+  }
+
+  filter(filters) {
+    const { type, words } = filters;
+    const [, ...rows] = this.table.getElementsByTagName("tr");
+    for (const row of rows) {
+      const [title, description, completed] = row.children;
+      let shouldHide = false;
+
+      if (words) {
+        shouldHide =
+          !title.innerText.includes(words) &&
+          !description.innerText.includes(words);
+      }
+
+      const shouldBeCompleted = type === "completed";
+      const isCompleted = completed.children[0].checked;
+
+      if (type !== "all" && shouldBeCompleted !== isCompleted) {
+        shouldHide = true;
+      }
+
+      if (shouldHide) {
+        row.classList.add("d-none");
+      } else {
+        row.classList.remove("d-none");
+      }
+    }
   }
 
   addTodo(title, description) {
@@ -32,12 +63,11 @@ export default class View {
   }
 
   editTodo(id, values) {
-      this.model.editTodo(id,values);
-      const row = document.getElementById(id);
-      row.children[0].innerText = values.title;
-      row.children[1].innerText = values.description;
-      row.children[2].children[0].checked = values.completed;
-
+    this.model.editTodo(id, values);
+    const row = document.getElementById(id);
+    row.children[0].innerText = values.title;
+    row.children[1].innerText = values.description;
+    row.children[2].children[0].checked = values.completed;
   }
 
   removeTodo(id) {
@@ -68,12 +98,13 @@ export default class View {
     editBtn.innerHTML = '<i class="fa fa-pencil"></i>';
     editBtn.setAttribute("data-toggle", "modal");
     editBtn.setAttribute("data-target", "#modal");
-    editBtn.onclick = () => this.modal.setValues({
+    editBtn.onclick = () =>
+      this.modal.setValues({
         id: todo.id,
         title: row.children[0].innerText,
         description: row.children[1].innerText,
         completed: row.children[2].children[0].checked,
-    });
+      });
     row.children[3].appendChild(editBtn);
 
     const removeBtn = document.createElement("button");
